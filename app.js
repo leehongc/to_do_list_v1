1f8f5f0ef2acc4d626c55653b18ec3b1a9af1901
Hong <leehongc@gmail.com>
2022-01-30 21:52:58 -0800

    complete to do list that takes user input

--- app.js
+++ app.js
@@ -6,45 +6,29 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = [];
 
 app.get("/", function(req, res) {
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-      default:
-      console.log("Error: current day is equal to: " + currentDay);
-  };
+  var options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric' };
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day});
-
-
-
+  res.render("list", {kindOfDay: day, newListItems: items});
 });
 
+app.post("/", function(req, res) {
+var item = req.body.newItem;
+items.push(item);
+
+res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.")
